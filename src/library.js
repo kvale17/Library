cf67184f198d3968c84aaf309785d9edeb082554
kvale17 <kevin.valencia24@gmail.com>
2024-04-23 14:38:43 -0700

    Add methods to add and save books in library

---
+++ src/library.js
@@ -0,0 +1,14 @@
+const myLibrary = [];
+
+function Book(name, author, pages, read) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary(name, author) {
+    const book = new Book(name, author);
+    myLibrary.push(book);
+}
+
